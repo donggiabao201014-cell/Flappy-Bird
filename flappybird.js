d617dcbc6052c55c4157b0895b06ca4e29c2ca61
donggiabao201014-cell <donggiabao201014@gmail.com>
2026-06-14 12:40:23 +0700

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -5,8 +5,8 @@ let boardHeight = 640;
 let context;
 
 //bird
-let birdWidth = 34; //width/height ratio = 408/228 = 17/12
-let birdHeight = 24;
+let birdWidth = 34 -34; //width/height ratio = 408/228 = 17/12
+let birdHeight = 24 -24;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
 let birdImg;
